a5638f1426c299fbae66db9be5454ee63f7cb08b
sudhanshuiist <63699822+sudhanshuiist@users.noreply.github.com>
2020-10-12 17:21:00 +0530

    Update camerascript.js

--- js/camerascript.js
+++ js/camerascript.js
@@ -27,6 +27,8 @@ function controlRecording(key) {
         mediaRecorder.ondataavailable = function(event){
           if (event.data.size > 0) {
             recordedChunks.push(event.data);
+          } else {
+            console.log(event);
           }
         };
 
@@ -48,21 +50,19 @@ function controlRecording(key) {
       track.stop();
     });
 
-    var blob = new Blob(recordedChunks, {'type' : 'video/webm;' });
-    
-    recordedChunks = [];
+    mediaRecorder.onstop = function(){
+      var blob = new Blob(recordedChunks, {'type': 'video/webm;'});
 
-    var url = URL.createObjectURL(blob);
-    downloadVideo.href = url;
-    downloadVideo.download = "video.webm";
-    downloadVideo.click();
-    window.URL.revokeObjectURL(url);
+      recordedChunks = [];
+  
+      var url = URL.createObjectURL(blob);
+      downloadVideo.href = url;
+      downloadVideo.download = "video.webm";
+      downloadVideo.click();
+      window.URL.revokeObjectURL(url);
+    }
 
   }
-
-  setTimeout(function(){
-    mediaRecorder.stop();
-  }, 5000);
 
 }
 
@@ -72,36 +72,3 @@ function retake() {
   stopVideo.hidden = true;
   retakeVideo.hidden = true;
 }
-
-// function shareToFacebook() {
-//   var imageData = canvas.toDataURL("images/png");
-
-//   if (window.XMLHttpRequest) {
-//     xmlhttp = new XMLHttpRequest();
-//   } else {
-//     xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-//   }
-
-//   xmlhttp.onreadystatechange = function () {
-//     if (this.readyState == 4 && this.status == 200) {
-
-//       var response = JSON.parse(this.responseText);
-
-//       if (response.status == 200) {
-//         var width = 600;
-//         var height = 400;
-//         var left = (screen.width - width) / 2;
-//         var top = (screen.height - (height + 100)) / 2;
-//         window.open(response.url, "Share to Facebook", 'width=' + width + ' , height=' + height + ' , top=' + top + ' , left=' + left);
-//       } else {
-//         console.log(this.responseText);
-//       }
-
-//     }
-//   }
-//   var form = new FormData;
-//   form.append('imageData', imageData);
-//   xmlhttp.open("POST", "graph.php");
-//   xmlhttp.send(form);
-//   return false;
-// }
